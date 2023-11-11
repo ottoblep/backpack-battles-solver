6629fd7b86b53ba6a13f8d8592fb3595b83e6887
Severin Lochschmidt <seviron53@gmail.com>
2023-11-11 19:48:58 +0100

    Improve variable naming

--- main.m
+++ main.m
@@ -36,32 +36,32 @@ disp(objectiveFunction(blocklist));
 
 function value = objectiveFunction(blocklist)
     value = 0;
-    adjacencyMatrix = checkBlockConnections(blocklist);
-    for item = 1:length(blocklist)
-        itemMultiplier = 1;
+    adjacency_matrix = checkBlockConnections(blocklist);
+    for item_idx = 1:length(blocklist)
+        item_multiplier = 1;
         % Check for modifiers from other items
         for i = 1:length(blocklist)
-            if adjacencyMatrix(item, i) ~= 1; continue; end
-            itemMultiplier = itemMultiplier * blocklist(i).connection_modifier;
+            if adjacency_matrix(item_idx, i) ~= 1; continue; end
+            item_multiplier = item_multiplier * blocklist(i).connection_modifier;
         end
-        value = value + (blocklist(item).value * itemMultiplier);
+        value = value + (blocklist(item_idx).value * item_multiplier);
     end
 end 
 
-function adjacencyMatrix = checkBlockConnections(blocklist)
-    adjacencyMatrix = zeros(length(blocklist));
+function adjacency_matrix = checkBlockConnections(blocklist)
+    adjacency_matrix = zeros(length(blocklist));
     % j block that connects, i block that is connected to
     % Connections are one-directional 
     for j = 1:length(blocklist)
         for i = 1:length(blocklist)
             if i==j; continue; end
             % Check each possible connection location of j
-            for connectionIdx = 1:length(blocklist(j).connection_blocks) 
-                connectionBlockPosition = blocklist(j).position + rotateBlock(blocklist(j).connection_blocks(connectionIdx,:), blocklist(j).rotation);
+            for connection_idx = 1:length(blocklist(j).connection_blocks) 
+                connection_block_position = blocklist(j).position + rotateBlock(blocklist(j).connection_blocks(connection_idx,:), blocklist(j).rotation);
                 % Check for overlap with each block of item i
                 for k = 1:size(blocklist(i).blocks,1)
-                    if connectionBlockPosition == blocklist(i).position + rotateBlock(blocklist(i).blocks(k,:), blocklist(i).rotation);
-                        adjacencyMatrix(i,j) = 1;
+                    if connection_block_position == blocklist(i).position + rotateBlock(blocklist(i).blocks(k,:), blocklist(i).rotation);
+                        adjacency_matrix(i,j) = 1;
                         break;
                     end
                 end
