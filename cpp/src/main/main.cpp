c4399404e5797225f77901e824a424337ca514c1
Severin Lochschmidt <seviron53@gmail.com>
2023-11-25 16:08:23 +0100

    Declare essential functions

--- cpp/src/main/main.cpp
+++ cpp/src/main/main.cpp
@@ -2,14 +2,36 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <array>
+#include <tuple>
 
 #include "items.h"
 #include "bags.h"
 
+using std::vector;
+using std::array;
+
+// This represents a matrix the size of the maximum grid possible
+typedef array<array<int, 7>, 9> GridMatrix;
+
+std::tuple<vector<Bag>, vector<Item>> randomPlacement(vector<Bag> baglist, vector<Item> itemlist){
+  return std::make_pair(baglist, itemlist);
+}
+
+GridMatrix generatePlacementMatrix(vector<Bag> baglist, vector<Item> itemlist){
+  GridMatrix result = {};
+  return result;
+}
+
 int main(int argc, char** argv) {
 
-  std::vector<Bag> baglist = {LookupBag("RangerBag"), LookupBag("RangerBag")};
-  std::vector<Item> itemlist = {LookupItem("WoodenSword"), LookupItem("Pan")};
+  vector<Bag> baglist = {LookupBag("RangerBag"), LookupBag("RangerBag")};
+  vector<Item> itemlist = {LookupItem("WoodenSword"), LookupItem("Pan")};
+
+  std::tie(baglist, itemlist) = randomPlacement(baglist, itemlist);
+
+  // Check validity of configuration
+  GridMatrix placement_matrix = generatePlacementMatrix(baglist, itemlist);
 
   std::cout << "Test";
   return EXIT_SUCCESS;
