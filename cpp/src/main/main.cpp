f437ea294644d536423a947103ca8f670548b7a4
Severin Lochschmidt <seviron53@gmail.com>
2024-01-31 15:06:44 +0100

    Raise complexity and compare both algorithms nicely

--- cpp/src/main/main.cpp
+++ cpp/src/main/main.cpp
@@ -19,12 +19,15 @@ int main(int argc, char** argv) {
   std::srand(std::time(nullptr)); // use current time as seed for random generator
 
   // Define item and bag inventory
-  vector<Bag> baglist = {LookupBag("RangerBag"), LookupBag("RangerBag"), LookupBag("StaminaSack"),};
-  vector<Item> itemlist = {LookupItem("WoodenSword"), LookupItem("Pan"), LookupItem("GlovesOfHaste")};
+  vector<Bag> baglist = {LookupBag("RangerBag"), LookupBag("PotionBelt"),  LookupBag("RangerBag"), LookupBag("StaminaSack"),};
+  vector<Item> itemlist = {LookupItem("Pan"), LookupItem("WoodenSword"), LookupItem("Pan"), LookupItem("GlovesOfHaste"), LookupItem("GlovesOfHaste")};
 
   // Choose a search strategy
-  // gridmatrix search_result = randomSearchStrategy(baglist, itemlist, 60, 11);
-  gridmatrix search_result = greedySearchStrategy(baglist, itemlist, 60, 11);
+  
+  gridmatrix search_result = greedySearchStrategy(baglist, itemlist, 5, 11);
+
+  global_best_score = 0;
+  search_result = randomSearchStrategy(baglist, itemlist, 60, 11);
 
   // If no valid configuration is found this will be all zeros
   printGridMatrix(search_result);
